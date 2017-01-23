0113e1e84a37c9de87ccab06c0343c08a753418e
ronnie wang <ro_wsy@163.com>
2017-01-23 17:32:28 +0800

    log request process time and id

--- src/main/java/wang/ronnie/RequestInterceptor.java
+++ src/main/java/wang/ronnie/RequestInterceptor.java
@@ -11,18 +11,24 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RequestInterceptor extends HandlerInterceptorAdapter {
 
+    public static int requestCount = 0;
+
+    private static final String RONNIE_WANG_REQUEST_ID = "ronnie.wang.request.id";
+
     private static final String RONNIE_WANG_REQUEST_START_TIME = "ronnie.wang.request.startTime";
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
-        request.setAttribute(RONNIE_WANG_REQUEST_START_TIME, System.currentTimeMillis());
+        long currentTimeMillis = System.currentTimeMillis();
+        request.setAttribute(RONNIE_WANG_REQUEST_ID, currentTimeMillis + (++requestCount % 10));
+        request.setAttribute(RONNIE_WANG_REQUEST_START_TIME, currentTimeMillis);
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
 
-        System.out.println(System.currentTimeMillis() - (long) request.getAttribute(RONNIE_WANG_REQUEST_START_TIME));
+        System.out.println(String.format("request id {%s}, request mills {%s}", request.getAttribute(RONNIE_WANG_REQUEST_ID), request.getAttribute(RONNIE_WANG_REQUEST_START_TIME)));
     }
 }
